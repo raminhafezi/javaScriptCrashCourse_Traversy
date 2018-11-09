c0c7e9fdb47a0f73c3973c8e50d6f7e84442f36f
raminhafezi <rmainhafezi@gmail.com>
2018-11-09 17:34:58 +1100

    more work on Cash Expense Report

--- CashExpenseReport/report.js
+++ CashExpenseReport/report.js
@@ -2,34 +2,55 @@ var fullName = document.getElementById("fullNameInputBox")
 fullName.addEventListener("blur", myFunction)
 
 
-function myFunction()
-{   var icon_left = document.getElementById("exclamation-icon")
-    var check_icon = document.getElementsByClassName("icon is-small is-right")[0]
-    console.log(check_icon.firstElementChild)
+function myFunction(){   
+    var check_icon = document.getElementById("check-icon")
+    // console.log(check_icon)
     var input_content = document.getElementById("fullNameInputBox").value
-    if (/^(([A-Za-z]+[\-\']?)*([A-Za-z]+)?\s)+([A-Za-z]+[\-\']?)*([A-Za-z]+)?$/.test(input_content)){
-        icon_left.classList.remove("has-text-danegr")
-        icon_left.classList.add("has-text-success")
-        check_icon.firstElementChild.classList.remove("has-text-danger")
-        check_icon.firstElementChild.classList.add("has-text-danger")
+    fullNameString = String(input_content.trim())
+    // console.log(input_content)
+    if (/^(([A-Za-z]+[\-\']?)*([A-Za-z]+)?\s)+([A-Za-z]+[\-\']?)*([A-Za-z]+)?$/.test(fullNameString)){
+        check_icon.classList.remove("has-text-danger")
+        check_icon.classList.remove("fa-spinner")
+        check_icon.classList.remove("fa-pulse")
+        check_icon.classList.add("fa-check")
+        check_icon.classList.add("has-text-success")
+        // console.log(check_icon.classList)
+    }
+    else {
+        check_icon.classList.add("has-text-danger")
+        check_icon.classList.add("fa-spinner")
+        check_icon.classList.add("fa-pulse")
+        check_icon.classList.remove("fa-check")
+        check_icon.classList.remove("has-text-success")
     }
 }
 
-var fullName = document.getElementById("email")
-fullName.addEventListener("blur", myEmailFunction)
+var Email = document.getElementById("emailBox")
+Email.addEventListener("blur", myEmailFunction)
 
 
-function myEmailFunction()
-{   var icon_left = document.getElementById("exclamation-icon")
-    var check_icon = document.getElementsByClassName("icon is-small is-right")[0]
-    var input_content = document.getElementById("email").value
-    console.log("Here in 2nd"+input_content)
-    if (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(String(input_content).toLocaleLowerCase())){
-        icon_left.classList.remove("has-text-danger")
-        console.log("I am here")
-        icon_left.classList.add("has-text-success")
+function myEmailFunction(){   
+    var email_icon = document.getElementById("email-icon")
+    console.log(email_icon)
+    var input_content = document.getElementById("emailBox").value
+    input_content = String(input_content.trim().toLowerCase())
+    if (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(input_content)){
+        email_icon.firstElementChild.classList.remove("fa-envelope")
+        email_icon.firstElementChild.classList.remove("has-text-danger")
+        email_icon.firstElementChild.classList.add("fa-check")
+        email_icon.firstElementChild.classList.add("has-text-success")
+    }
+    else{
+        email_icon.firstElementChild.classList.remove("fa-check")
+        email_icon.firstElementChild.classList.remove("has-text-success")
+        email_icon.firstElementChild.classList.add("fa-envelope")
+        email_icon.firstElementChild.classList.add("has-text-danger")
         
-        check_icon.firstElementChild.classList.remove("has-text-danger")
-        check_icon.firstElementChild.classList.add("has-text-success")
+
     }
 }
+
+
+
+// fa fa-spinner fa-pulse
+// fa-check
